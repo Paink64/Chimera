0285e301bd6737c0cf406958ab3809f438f54a8e
Joaquin Robles <roblesjack@hotmail.com>
2022-04-23 00:42:12 -0700

    Files

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -8,12 +8,13 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 Amplify.configure(awsconfig)
 
 function MainPage() {
+  
   return (
     <div className="App">
       <header>
         <h1>We now have Auth!</h1>
-        {/*<MainPage/>*/}
       </header>
+      
       <AmplifySignOut />
     </div>
   );
