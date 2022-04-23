e02c8b7ee36e0443f2e8bf4acbb16503f17efea9
Joaquin Robles <roblesjack@hotmail.com>
2022-04-23 03:05:18 -0700

    Event

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import './App.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-<<<<<<< HEAD
 import Create from './components/Create/Create.js';
-import Test from "./components/Test"
-=======
-import MainPage from './components/MainPage';
 import Test from "./components/Test";
 import Calendar from "./components/Calander/Calander";
->>>>>>> 0c293958e6a3f61ad0c1fefc7f1ac9a79fa8c466
 
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 
@@ -22,13 +17,9 @@ function App() {
    
     <Routes>
       
-<<<<<<< HEAD
       
       <Route path="/" exact element={<Create/>}/>
-=======
-      <Route path="/" exact element={<MainPage/>}/>
       <Route path="/Calendar" exact element={<Calendar/>}/>
->>>>>>> 0c293958e6a3f61ad0c1fefc7f1ac9a79fa8c466
       <Route path="/Test" exact element={<Test/>}/>
       
       
